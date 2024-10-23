2b080d5063247ce2a115896ae7ee8a4681e3ace3
saraborg01 <borgsar@ed-3358-14.studat.chalmers.se>
2024-10-23 14:32:09 +0200

    lab3 fixes

--- Lab2/pintos/src/devices/batch-scheduler.c
+++ Lab2/pintos/src/devices/batch-scheduler.c
@@ -56,6 +56,14 @@ typedef struct {
   unsigned long transfer_duration;
 } task_t;
 
+
+/* Globals*/
+static direction_t current_bus_dir; // Bus direction
+static int tasks_on_bus; // Number of tasks on bus
+static struct lock bus_lock; // Lock for the bus
+static struct condition wait_dir_cond[NUM_OF_DIRECTIONS][NUM_OF_PRIORITIES] = {0}; // Conditions for directions and priority pairs
+static int waiting_tasks[NUM_OF_DIRECTIONS][NUM_OF_PRIORITIES] = {0}; // Count array for conditions 
+
 void init_bus (void);
 void batch_scheduler (unsigned int num_priority_send,
                       unsigned int num_priority_receive,
@@ -76,29 +84,20 @@ static void transfer_data (const task_t *task);
 /* Releases the slot */
 static void release_slot (const task_t *task);
 
-static struct lock bus_lock;
-static struct condition cond[NUM_OF_DIRECTIONS][NUM_OF_PRIORITIES];
-static int current_task_direction;
-static int tasks_on_bus;
-static int waiting_tasks[NUM_OF_DIRECTIONS][NUM_OF_PRIORITIES];
-
 void init_bus (void) {
 
   random_init ((unsigned int)123456789);
 
-  /* TODO: Initialize global/static variables,
-     e.g. your condition variables, locks, counters etc */
-  lock_init(&bus_lock);
+  tasks_on_bus = 0; // No tasks on bus
+  current_bus_dir = SEND; // Initally send
+  lock_init(&bus_lock); // Init the lock
 
-  for (int d = 0; d < NUM_OF_DIRECTIONS; d++){
-    for (int p = 0; p < NUM_OF_PRIORITIES; p++){
-      cond_init(&cond[d][p]);
-      waiting_tasks[d][p] = 0;
+  //Init all conditions in the wait_dir_cond array:
+  for (int dir = 0; dir < NUM_OF_DIRECTIONS; dir++){
+    for (int prio = 0; prio < NUM_OF_PRIORITIES; prio++){
+      cond_init(&wait_dir_cond[dir][prio]);
     }
-  }
-
-  current_task_direction = -1;
-  tasks_on_bus = 0;
+  }  
 }
 
 void batch_scheduler (unsigned int num_priority_send,
@@ -193,23 +192,23 @@ static direction_t other_direction(direction_t this_direction) {
 }
 
 void get_slot (const task_t *task) {
-  direction_t direction = task->direction;
-  priority_t priority  = task->priority;
-  direction_t other_dir = (direction==SEND) ? RECEIVE : SEND;
+  direction_t current_dir = task->direction;
+  priority_t current_prio = task->priority;
 
-  lock_acquire(&bus_lock);
-  while((tasks_on_bus == BUS_CAPACITY) || 
-        (tasks_on_bus > 0 && task->direction != current_task_direction ) || 
-        (priority == NORMAL && (waiting_tasks[direction][PRIORITY] > 0 || waiting_tasks[other_dir][PRIORITY] > 0))){
-      
-      waiting_tasks[direction][priority]++;
-      cond_wait(&cond[direction][priority], &bus_lock);
-      waiting_tasks[direction][priority]--;
+  lock_acquire(&bus_lock); // Acquire the lock
+  while((tasks_on_bus == BUS_CAPACITY) || (tasks_on_bus > 0 && current_bus_dir == other_direction(current_dir))
+  | ((current_prio == NORMAL) && (waiting_tasks[other_direction(current_dir)][PRIORITY]>0))){
+    // Block acces if:
+    // - Buss full
+    // - Tasks in other direction
+    // - Task in other direction has Priority
+    waiting_tasks[current_dir][current_prio]++; // Inc. waiting tasks in dir, prio
+    cond_wait(&wait_dir_cond[current_dir][current_prio], &bus_lock); // Wait on condition
+    waiting_tasks[current_dir][current_prio]--; // Dec. waiting tasks in dir, prio
   }
-
-  tasks_on_bus++;
-  current_task_direction = direction;
-  lock_release(&bus_lock);
+  tasks_on_bus++; // Add task on bus
+  current_bus_dir = current_dir; // Change the bus direction to that of the task
+  lock_release(&bus_lock); // Relelase the lock
 }
 
 void transfer_data (const task_t *task) {
@@ -218,34 +217,28 @@ void transfer_data (const task_t *task) {
 }
 
 void release_slot (const task_t *task) {
-  direction_t direction = task->direction;
-  direction_t other_dir = (direction==SEND) ? RECEIVE : SEND;
-  
-  lock_acquire(&bus_lock);
-  
-  tasks_on_bus--;
 
-  if (tasks_on_bus == 0){
-    if (waiting_tasks[direction][PRIORITY] > 0){
-      cond_broadcast(&cond[direction][PRIORITY], &bus_lock);
-    }
-    else if (waiting_tasks[other_dir][PRIORITY] > 0){
-      cond_broadcast(&cond[other_dir][PRIORITY], &bus_lock);
-    }
-    else if (waiting_tasks[direction][NORMAL] > 0){
-      cond_broadcast(&cond[direction][NORMAL], &bus_lock);
-    }
-    else if (waiting_tasks[other_dir][NORMAL] > 0){
-      cond_broadcast(&cond[other_dir][NORMAL], &bus_lock);
-    }
+  direction_t current_dir = task->direction;
+  direction_t other_dir = other_direction(current_dir);
+
+  lock_acquire(&bus_lock); // Acquire the lock
+  tasks_on_bus--; // Remove task from the bus
+  
+  if(waiting_tasks[current_dir][PRIORITY] > 0 ){
+    // If there is a task waiting in direction with priority
+    cond_signal(&wait_dir_cond[current_dir][PRIORITY],&bus_lock);
   } 
-  else {
-    if (waiting_tasks[direction][PRIORITY]){
-      cond_signal(&cond[direction][PRIORITY], &bus_lock);
-    }
-    else if (waiting_tasks[other_dir][PRIORITY] == 0){
-      cond_signal(&cond[direction][NORMAL], &bus_lock);
-    }
+  else if((waiting_tasks[other_dir][PRIORITY] > 0 ) && (tasks_on_bus == 0)){
+    // If there is a task waiting in the other direction with priority
+    cond_signal(&wait_dir_cond[other_dir][PRIORITY],&bus_lock);
   }
-  lock_release(&bus_lock);
+  else if(waiting_tasks[current_dir][NORMAL] > 0 ){
+    // If there is a task in current direction without priority
+    cond_signal(&wait_dir_cond[current_dir][NORMAL], &bus_lock);
+  } 
+  else if((waiting_tasks[other_dir][NORMAL] > 0 ) && (tasks_on_bus == 0)){
+    // If there is a task waiting in the other direction without priority
+    cond_signal(&wait_dir_cond[other_dir][NORMAL], &bus_lock);
+  }
+  lock_release(&bus_lock); // Release lock
 }
